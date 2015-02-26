f2e5d5fbe77682a6d9983c96ae202f81b60ad4e7
George Boyle <george@thebuds.net>
2015-02-26 23:36:18 +0000

    Minor change to test continuous delivery

--- src/Main.hs
+++ src/Main.hs
@@ -18,7 +18,7 @@ site :: Snap ()
 site =
     ifTop (writeBS "hello world") <|>
     route [ ("foo", writeBS "bar")
-          , ("budget", writeBS "You can fuck off")
+          , ("budget", writeBS "I said fuck off")
           , ("budget/json", budgetHandler)
           , ("echo/:echoparam", echoHandler)
           ] <|>
