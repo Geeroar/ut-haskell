de4f8b3715cd7e9661494b9df43e3100158c418b
Ruairi O Brien <ruairitobrien@gmail.com>
2015-06-13 16:15:03 +0100

    Little bit of reorganising code.

--- src/Main.hs
+++ src/Main.hs
@@ -1,53 +1,6 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Budget
-import Control.Applicative
-import Data.Aeson
-import qualified Data.ByteString.Lazy as Lz
-import Snap.Core
-import Snap.Util.FileServe
-import Snap.Http.Server
-import Types
+import qualified Server as S
 
 main :: IO ()
-main = quickHttpServe site
-
-site :: Snap ()
-site =
-    ifTop (writeBS "hello world") <|>
-    route [ ("foo", writeBS "bar")
-          , ("budget", writeBS "You can fook off")
-          , ("budget/json", budgetHandler)
-          , ("echo/:echoparam", echoHandler)
-          ] <|>
-    dir "static" (serveDirectory ".")
-
-responseIntercept :: Response -> Response
-responseIntercept = setContentType "application/json"
-
-badRequest :: Response -> Response
-badRequest = setResponseCode 400
-
-budgetHandler :: Snap ()
-budgetHandler = do
-    body <- readRequestBody 1000
-    modifyResponse responseIntercept
-    doResponse $ decodeBudgetRequest body
-
-decodeBudgetRequest :: Lz.ByteString -> Maybe BudgetRequest
-decodeBudgetRequest = decode
-
-doResponse :: Maybe BudgetRequest -> Snap ()
-doResponse resp = do
-    case resp of
-        Just r  -> writeLBS $ encode $ budget r
-        Nothing -> do
-            modifyResponse badRequest
-            writeLBS "{\"error\": \"You can fuck off\"}"
-
-echoHandler :: Snap ()
-echoHandler = do
-    param <- getParam "echoparam"
-    maybe (writeBS "must specify echo/param in URL")
-          writeBS param
+main = S.main
